ef741a5ed1711533d587f2e6c7f33e6d65daf36a
scorpion2128 <alexander.junnior.chacchi.luque@gmail.com>
2025-01-12 14:22:51 -0500

    Add React components and related files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Puedes usar este archivo para estilos globales si es necesario
+import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
